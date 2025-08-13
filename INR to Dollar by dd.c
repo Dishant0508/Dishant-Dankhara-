5fbf1c8c305838bd417859cc1fb5eaac18163d24
Dishant0508 <dankharajyot@gmail.com>
2025-08-13 12:44:16 +0530

    Add files via upload

---
+++ INR to Dollar by dd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float i,d;
+    printf("\n convert INR to Doller  ");
+    printf("enter the INR :");
+    scanf("%f",&i);
+    d=i/48;
+    printf("\n your minutes is : %f",d);
+    getch();
+
+}
