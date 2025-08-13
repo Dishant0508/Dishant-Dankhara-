6aec93ed8630e8b308d6719c957f8c6f27963c02
Dishant0508 <dankharajyot@gmail.com>
2025-08-13 12:33:58 +0530

    Add files via upload

---
+++ min to hours by dd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float h,m,a;
+    printf("\n convert to min into hour ");
+    printf("enter the min :");
+    scanf("%f",&m);
+    h=m/60;
+    printf("\n hours is : %f",h);
+    getch();
+
+}
