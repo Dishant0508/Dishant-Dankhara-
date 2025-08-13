b38537abfcbaada1a5242560637674df972227e0
Dishant0508 <dankharajyot@gmail.com>
2025-08-13 11:54:06 +0530

    Add files via upload

---
+++ multiply by dd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num1,num2,num3;
+    printf("/n enter the num 1:");
+    scanf("%d",&num1);
+    printf("/n enter the num 2:");
+    scanf("%d",&num2);
+    num3=num1*num2;
+    printf("/n %d*%d=%d:",num1,num2,num3);
+    getch();
+}
